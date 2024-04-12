4e8e2c1cddcb0a90549b2464025e6d13c44c12db
Priti Gupta <civilstudent9900@gmail.com>
2024-04-12 12:12:41 +0530

    fullstack course 1 push

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Hiiiiiiiiiiiiii
+    </div>
+  )
+}
+
+export default page
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,22 @@
+
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("Mongodb connected successfully")
+    })
+
+    connection.on("error", (err) => {
+      console.log("Mongodb connection error" + err)
+      process.exit();
+    })
+
+  } catch (error) {
+    console.log("Somthing went wrong!")
+    console.log(error)
+  }
+}
